fe1079c2cb7669dc5d8674cf20e8c9c2e125bef9
mosecoza <mosecoza@gmail.com>
2016-09-26 14:19:15 +0200

    this is a second commit -added a toast message for negative and over 100 coffees - the order summary is sent to the default email through an intent so it could be sent to the coffee shop -

--- app/src/main/java/za/co/example/admin/justjava/MainActivity.java
+++ app/src/main/java/za/co/example/admin/justjava/MainActivity.java
@@ -1,11 +1,14 @@
 package za.co.example.admin.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         if (quantity == 100.0) {
+            Toast.makeText(this, "We take orders in 100s", Toast.LENGTH_SHORT).show();
             return;
         }
         quantity = quantity + 1;
@@ -39,17 +43,21 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         if (quantity == 1) {
+            Toast.makeText(this, "Please order atleast one coffee", Toast.LENGTH_SHORT).show();
             return;
         }
         quantity = quantity - 1;
         display(quantity);
     }
 
+
+
     /**
      * This method displays and confirm the price.
      */
     public void submitOrder(View view) {
-        String name=displayName(setxt);
+            EditText txtname=(EditText)findViewById(R.id.editxt_name) ;
+            String name = txtname.getText().toString();
         CheckBox wc = (CheckBox) findViewById(R.id.whip_cream_chechBox);
         boolean hasWhippedcream = wc.isChecked();
 
@@ -57,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         boolean hasChocolate = cl.isChecked();
 
         double price = calculatePrice(quantity);
-        String message = "ORDER SUMMARY\n\n";
+        String message = "ORDER SUMMARY\n";
         message += "\nName: " + name  ;
         if (hasWhippedcream) {
             message += "\nAdd whipped cream.";
@@ -69,16 +77,16 @@ public class MainActivity extends AppCompatActivity {
         message += "\nTotal: R" + price;
         message += "\nThank you!";
         display(quantity);
-        displayMessage(message);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just java order for "+ name);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 
-    /**
-     * This method is used to display the price.
-     */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
 
     /**
      * This method displays the given quantity value on the screen.
@@ -98,13 +106,5 @@ public class MainActivity extends AppCompatActivity {
         return num;
     }
 
-    /**
-     * This method is used to display the name of the person placing an order.
-     */
-    private String displayName(String nam) {
-        EditText txtname=(EditText)findViewById(R.id.editxt_name) ;
 
-        nam = txtname.getText().toString();
-        return nam;
-    }
 }
